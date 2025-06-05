ec590dd33cc740e90105c22a5f29c2822d98bbfd
Kwon-HyeongIl <llioopv@naver.com>
2025-06-05 10:21:33 +0900

    Initial Commit

---
+++ Jamjam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jamjam
+//
+//  Created by 권형일 on 6/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jamjam/JamjamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamjamApp.swift
+//  Jamjam
+//
+//  Created by 권형일 on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct JamjamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
