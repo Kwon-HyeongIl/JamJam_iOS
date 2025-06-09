86238d267fffcf6ebc413de8927ef34858090f8c
Kwon-HyeongIl <llioopv@naver.com>
2025-06-09 13:03:28 +0900

    feat: Add SplashView

---
+++ Jamjam/Component/Splash/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Jamjam
+//
+//  Created by 권형일 on 6/9/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
--- Jamjam/ContentView.swift
+++ Jamjam/ContentView.swift
@@ -8,7 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isSplashVisible = true
+    
     var body: some View {
-        MainTabView()
+        ZStack {
+            MainTabView()
+            
+            if isSplashVisible {
+                SplashView()
+                    .animation(.easeOut(duration: 0.4), value: isSplashVisible)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    isSplashVisible = false
+                }
+            }
+        }
     }
 }
