b76bd15198f165886c048f52c722805c949c17fd
Kwon-HyeongIl <llioopv@naver.com>
2025-06-16 14:32:49 +0900

    feat: Add SignUpView with expert and client

---
+++ Jamjam/View (V)/Auth/SignUp/Expert/ExpertSignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpertSignUpView.swift
+//  Jamjam
+//
+//  Created by 권형일 on 6/16/25.
+//
+
+import SwiftUI
+
+struct ExpertSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpertSignUpView()
+}
---
+++ Jamjam/View (V)/Auth/SignUp/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Jamjam
+//
+//  Created by 권형일 on 6/16/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
