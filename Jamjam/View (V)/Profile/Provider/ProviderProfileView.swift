353a2c46fc00842ce0249eaf77d29192f68b52b6
Kwon-HyeongIl <llioopv@naver.com>
2025-07-02 02:17:49 +0900

    feat: Add ProviderProfileView UI

--- Jamjam/View (V)/Profile/Provider/ProviderProfileView.swift
+++ Jamjam/View (V)/Profile/Provider/ProviderProfileView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ProviderProfileView: View {
     @State private var viewModel = ProviderProfileViewModel()
+    
+    @State private var position = ScrollPosition()
+    
     @Namespace private var tabListUnderline
     
     var body: some View {
@@ -77,9 +80,119 @@ struct ProviderProfileView: View {
                     // MARK: Tab Selection
                     LazyVStack(pinnedViews: [.sectionHeaders]) {
                         Section {
-                            Rectangle()
-                                .frame(width: 50, height: 1200)
-                                .foregroundStyle(.red.opacity(0.1))
+                            VStack {
+                                // MARK: 자기 소개
+                                VStack {
+                                    HStack {
+                                        Text("자기 소개")
+                                            .font(.pretendard(Pretendard.semiBold, size: 17))
+                                            .padding(.leading, 30)
+                                        
+                                        Spacer()
+                                    }
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                                    
+                                    HStack {
+                                        Text("안녕하세요! 저는 다양한 분야에서 경험을 쌓아온 전문가입니다. 고객의 니즈에 맞는 맞춤형 서비스를 제공하며, 항상 최고의 결과를 위해 노력하고 있습니다.")
+                                            .font(.pretendard(Pretendard.medium, size: 13))
+                                            .padding(.leading, 30)
+                                        
+                                        Spacer()
+                                    }
+                                }
+                                .padding(.bottom)
+                                
+                                // MARK: 보유 기술
+                                VStack {
+                                    HStack {
+                                        Text("보유 기술")
+                                            .font(.pretendard(Pretendard.semiBold, size: 17))
+                                            .padding(.leading, 30)
+                                        
+                                        Spacer()
+                                    }
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                                    
+                                    HStack {
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 70, height: 30)
+                                            .foregroundStyle(.white)
+                                            .overlay {
+                                                RoundedRectangle(cornerRadius: 20)
+                                                    .stroke(.gray.opacity(0.3), lineWidth: 1)
+                                            }
+                                            .padding(.leading, 30)
+                                        
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 70, height: 30)
+                                            .foregroundStyle(.white)
+                                            .overlay {
+                                                RoundedRectangle(cornerRadius: 20)
+                                                    .stroke(.gray.opacity(0.3), lineWidth: 1)
+                                            }
+                                        
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 70, height: 30)
+                                            .foregroundStyle(.white)
+                                            .overlay {
+                                                RoundedRectangle(cornerRadius: 20)
+                                                    .stroke(.gray.opacity(0.3), lineWidth: 1)
+                                            }
+                                        
+                                        Spacer()
+                                    }
+                                }
+                                .padding(.bottom)
+                                
+                                // MARK: 경력 사항
+                                VStack {
+                                    HStack {
+                                        Text("경력 사항")
+                                            .font(.pretendard(Pretendard.semiBold, size: 17))
+                                            .padding(.leading, 30)
+                                        
+                                        Spacer()
+                                    }
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                                    
+                                    HStack {
+                                        Image(systemName: "graduationcap.fill")
+                                            .font(.system(size: 12))
+                                            .foregroundStyle(.gray.opacity(0.5))
+                                            .padding(.leading, 30)
+                                        
+                                        Text("서울대학교 컴퓨터공학과 졸업")
+                                            .font(.pretendard(Pretendard.medium, size: 13))
+                                        
+                                        Spacer()
+                                    }
+                                }
+                                .padding(.bottom)
+                                
+                                Rectangle()
+                                    .frame(width: 50, height: 800)
+                                    .foregroundStyle(.red.opacity(0.1))
+                                
+                                // MARK: 제공 서비스
+                                VStack {
+                                    HStack {
+                                        Text("제공 서비스")
+                                            .font(.pretendard(Pretendard.semiBold, size: 17))
+                                            .padding(.leading, 30)
+                                        
+                                        Spacer()
+                                    }
+                                    .padding(.top, 10)
+                                    .padding(.bottom, 10)
+                                }
+                                
+                                Rectangle()
+                                    .frame(width: 50, height: 500)
+                                    .foregroundStyle(.red.opacity(0.1))
+                            }
                         } header: {
                             HStack {
                                 ForEach(Array(viewModel.tabBarList.enumerated()), id: \.offset) { idx, title in
@@ -107,6 +220,22 @@ struct ProviderProfileView: View {
                                             viewModel.selectedIndex = idx
                                         }
                                     }
+                                    .onChange(of: viewModel.selectedIndex) { _, newIndex in
+                                        if newIndex == 0 {
+                                            withAnimation(.spring(response: 0.2,
+                                                                  dampingFraction: 1,
+                                                                  blendDuration: 0)) {
+                                                position.scrollTo(edge: .top)
+                                            }
+                                            
+                                        } else if newIndex == 1 {
+                                            withAnimation(.spring(response: 0.2,
+                                                                  dampingFraction: 1,
+                                                                  blendDuration: 0)) {
+                                                position.scrollTo(edge: .bottom)
+                                            }
+                                        }
+                                    }
                                 }
                             }
                             .padding(.top)
@@ -116,6 +245,7 @@ struct ProviderProfileView: View {
                 }
             }
             .modifier(NavigationBarBackAndTitleAndHomeModifier(title: "전문가"))
+            .scrollPosition($position)
         }
     }
 }
