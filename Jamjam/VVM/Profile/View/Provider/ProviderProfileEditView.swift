52662e6a30efc378a28839416338499bf6719f9f
Kwon-HyeongIl <llioopv@naver.com>
2025-07-04 12:15:12 +0900

    feat: Add ProviderProfileEdit view and viewModel

---
+++ Jamjam/VVM/Profile/View/Provider/ProviderProfileEditView.swift
@@ -0,0 +1,29 @@
+//
+//  ProviderProfileEditView.swift
+//  Jamjam
+//
+//  Created by 권형일 on 7/4/25.
+//
+
+import SwiftUI
+
+struct ProviderProfileEditView: View {
+    @State private var viewModel = ProviderProfileEditViewModel()
+    
+    var body: some View {
+        MainBackground {
+            ScrollView(showsIndicators: false) {
+                
+            }
+            .modifierIf(viewModel.pageIndex == 0, then: {
+                NavigationBarBackAndTitleModifier(title: "프로필 설정")
+            }, else: {
+                NavigationBarTitleAndHomeModifier(title: "프로필 설정")
+            })
+        }
+    }
+}
+
+#Preview {
+    ProviderProfileEditView()
+}
---
+++ Jamjam/VVM/Profile/ViewModel/Provider/ProviderProfileEditViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProviderProfileEditViewModel.swift
+//  Jamjam
+//
+//  Created by 권형일 on 7/4/25.
+//
+
+import Foundation
+
+@Observable
+class ProviderProfileEditViewModel {
+    var pageIndex = 0
+}
